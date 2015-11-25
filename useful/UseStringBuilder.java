2c799ce1d8f385d551b4c01efb8e2b6520540ff1
Fred Yang <fredyangss@gmail.com>
2015-11-25 09:16:05 -0800

    string builder vs concat

---
+++ useful/UseStringBuilder.java
@@ -0,0 +1,26 @@
+level28ckage org.test;
+
+/**
+ * Someone asked why.
+ */
+public class App {
+    public static void main(String[] args) {
+        int numLoops = 100000;
+
+        // not a good way to 'benchmark', good enough here though
+        long startTime = System.currentTimeMillis();
+        String str = "";
+        for (int i = 0; i < numLoops; ++i) {
+            str += "0";
+        }
+        System.out.printf(System.currentTimeMillis() - startTime);
+
+        startTime = System.currentTimeMillis();
+        // buffer for sync, again, doesn't matter here (well, *not* using it matters here)
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < numLoops; ++i) {
+            strBuilder.append("0");
+        }
+        System.out.printf(System.currentTimeMillis() - startTime);
+    }
+}
