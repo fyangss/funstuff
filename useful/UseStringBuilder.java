659cc94ffa9dcd33c903b1f1076a1089f7d38b3b
fyangss <fredyangss@gmail.com>
2017-11-15 06:08:45 -0800

    Update UseStringBuilder.java

--- useful/UseStringBuilder.java
+++ useful/UseStringBuilder.java
@@ -2,13 +2,12 @@
  * Someone asked why.
  */
 public class UseStringBuilder {
+    private final static int NUM_LOOPS = 100000;
     public static void main(String[] args) {
-        int numLoops = 100000;
-
-        // not a good way to 'benchmark', good enough here though
+        // not a good way to 'benchmark' in general, good enough here
         long startTime = System.currentTimeMillis();
         String str = "";
-        for (int i = 0; i < numLoops; ++i) {
+        for (int i = 0; i < NUM_LOOPS; ++i) {
             str += "0";
         }
         System.out.println(System.currentTimeMillis() - startTime);
@@ -16,7 +15,7 @@ public class UseStringBuilder {
         startTime = System.currentTimeMillis();
         // buffer for sync, again, doesn't matter here (well, *not* using it matters here)
         StringBuilder strBuilder = new StringBuilder();
-        for (int i = 0; i < numLoops; ++i) {
+        for (int i = 0; i < NUM_LOOPS; ++i) {
             strBuilder.append("0");
         }
         System.out.println(System.currentTimeMillis() - startTime);
